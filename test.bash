ad076929974ce80675815d0efe0c8b19935eba23
Full Metal Panic! <x78618741@gmail.com>
2025-11-29 15:59:12 +0900

    テスト用

--- test.bash
+++ test.bash
@@ -8,13 +8,23 @@ ng () {
 }
 
 res=0
+tolerance=0.0001
 
+float_equal() {
+    a=$1
+    b=$2
+    diff=$(echo "$a - $b" | bc -l)
+    absdiff=$(echo "if ($diff<0) -1*$diff else $diff" | bc -l)
+    [ $(echo "$absdiff < $tolerance" | bc -l) -eq 1 ]
+}
 
 out=$(echo -e "40\n4\n5" | ./syodou)
-[ "${out}" = "0.4270707954918481" ] || ng ${LINENO}
+expected="0.4270707954918481"
+float_equal "$out" "$expected" || ng ${LINENO}
 
 out=$(echo -e "60\n6\n5" | ./syodou)
-[ "${out}" = "0.4209460676823561" ] || ng ${LINENO}
+expected="0.4209460676823561"
+float_equal "$out" "$expected" || ng ${LINENO}
 
 echo -e "40\n4" | ./syodou >/dev/null 2>&1
 [ $? -eq 1 ] || ng ${LINENO}
@@ -27,5 +37,6 @@ echo -e "あいうえお\n4\n5" | ./syodou >/dev/null 2>&1
 
 [ "$res" = 0 ] && echo OK
 exit $res
+
 
 
