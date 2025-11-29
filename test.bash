6b678ece96f8f1010d1d5b54811861eb93c89e1a
Full Metal Panic! <x78618741@gmail.com>
2025-11-29 15:20:48 +0900

    add

--- test.bash
+++ test.bash
@@ -1,48 +1,27 @@
-#!/bin/bash -xv
-#SPDX-FileCopyrightText: 2025 Matumoto Hayoto
+#!/bin/bash
+# SPDX-FileCopyrightText: 2025 Matumoto Hayoto
 # SPDX-License-Identifier: BSD-3-Clause
 
-err=0
+ng () {
+    echo ${1}行目: NG
+    res=1
+}
 
-output=$(echo -e "40\n4\n5" | ./syodou)
-expected="0.4270707954918481"
-if [ "$output" = "$expected" ]; then
-    echo "test1 OK"
-else
-    echo "test1 NG"
-    err=1
-fi
+res=0
 
-output=$(echo -e "60\n6\n5" | ./syodou)
-expected="0.4209460676823561"
-if [ "$output" = "$expected" ]; then
-    echo "test2 OK"
-else
-    echo "test2 NG"
-    err=1
-fi
+
+out=$(echo -e "40\n4\n5" | ./syodou)
+[ "${out}" = "0.4270707954918481" ] || ng ${LINENO}
+
+out=$(echo -e "60\n6\n5" | ./syodou)
+[ "${out}" = "0.4209460676823561" ] || ng ${LINENO}
 
 echo -e "40\n4" | ./syodou >/dev/null 2>&1
-if [ $? -ne 1 ]; then
-    echo "test3 NG"
-    err=1
-else
-    echo "test3 OK"
-fi
+[ $? -eq 1 ] || ng ${LINENO}
 
 echo -e "aaa\n4\n5" | ./syodou >/dev/null 2>&1
-if [ $? -ne 1 ]; then
-    echo "test4 NG"
-    err=1
-else
-    echo "test4 OK"
-fi
+[ $? -eq 1 ] || ng ${LINENO}
 
-if [ $err -eq 0 ]; then
-    echo "ALL TESTS OK"
-    exit 0
-else
-    echo "SOME TESTS FAILED"
-    exit 1
-fi
+[ "$res" = 0 ] && echo OK
+exit $res
 
