ca61db964296727d47100db0050ef0ffaa78b98a
Full Metal Panic! <x78618741@gmail.com>
2025-11-29 14:33:31 +0900

    add

--- test.bash
+++ test.bash
@@ -2,23 +2,38 @@
 #SPDX-FileCopyrightText: 2025 Matumoto Hayoto
 # SPDX-License-Identifier: BSD-3-Clause
 
-ng () {
-         echo ${1}行目が違うよ
-         res=1
-}
+#!/bin/bash
 
-res=0
+output=$(echo -e "40\n4\n5" | ./syodou)
+expected="0.40689655172413796"
+if [ "$output" != "$expected" ]; then
+    echo "test1: expected $expected but got $output"
+    exit 1
+fi
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+output=$(echo -e "60\n6\n5" | ./syodou)
+expected="0.45111731843575417"
+if [ "$output" != "$expected" ]; then
+    echo "test2: expected $expected but got $output"
+    exit 1
+fi
 
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+output=$(echo -e "40\n4" | ./syodou; echo $?)
+if [ "$output" = "1" ]; then
+    true
+else
+    echo "test3: expected exit 1 for insufficient input"
+    exit 1
+fi
 
-out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+output=$(echo -e "aaa\n4\n5" | ./syodou; echo $?)
+if [ "$output" = "1" ]; then
+    true
+else
+    echo "test4: expected exit 1 for invalid input"
+    exit 1
+fi
 
-[ "${res}" = 0 ] && echo OK
-exit $res
+echo "OK"
+exit 0
+
