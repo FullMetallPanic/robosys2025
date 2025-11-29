d78d0a5f7c7ffae0d425d3246f650f528fb43f74
Full Metal Panic! <x78618741@gmail.com>
2025-11-29 14:42:13 +0900

    add

--- test.bash
+++ test.bash
@@ -2,38 +2,47 @@
 #SPDX-FileCopyrightText: 2025 Matumoto Hayoto
 # SPDX-License-Identifier: BSD-3-Clause
 
-#!/bin/bash
+err=0
 
 output=$(echo -e "40\n4\n5" | ./syodou)
 expected="0.40689655172413796"
 if [ "$output" != "$expected" ]; then
-    echo "test1: expected $expected but got $output"
-    exit 1
+    echo "test1 NG"
+    err=1
+else
+    echo "test1 OK"
 fi
 
 output=$(echo -e "60\n6\n5" | ./syodou)
 expected="0.45111731843575417"
 if [ "$output" != "$expected" ]; then
-    echo "test2: expected $expected but got $output"
-    exit 1
-fi
-
-output=$(echo -e "40\n4" | ./syodou; echo $?)
-if [ "$output" = "1" ]; then
-    true
+    echo "test2 NG"
+    err=1
 else
-    echo "test3: expected exit 1 for insufficient input"
-    exit 1
+    echo "test2 OK"
 fi
 
-output=$(echo -e "aaa\n4\n5" | ./syodou; echo $?)
-if [ "$output" = "1" ]; then
-    true
+echo -e "40\n4" | ./syodou >/dev/null 2>&1
+if [ $? -ne 1 ]; then
+    echo "test3 NG"
+    err=1
 else
-    echo "test4: expected exit 1 for invalid input"
-    exit 1
+    echo "test3 OK"
 fi
 
-echo "OK"
-exit 0
+echo -e "aaa\n4\n5" | ./syodou >/dev/null 2>&1
+if [ $? -ne 1 ]; then
+    echo "test4 NG"
+    err=1
+else
+    echo "test4 OK"
+fi
+
+if [ $err -eq 0 ]; then
+    echo "ALL TESTS OK"
+    exit 0
+else
+    echo "SOME TESTS FAILED"
+    exit 1
+fi
 
